a41abe772e19c700d8965cf99a6f6d63d1bac592
zArthurOS <arthur426santos@gmail.com>
2024-08-31 20:06:06 -0300

    FindingNemo

---
+++ month01/Week01/Day02/Finding Nemo/Finding Nemo/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string sentence = "Dory forgot who's Nemo again.";
+
+FindingNemo FindingNemoClass = new();
+FindingNemoClass.findNemo(sentence);
+
+Console.ReadKey();
+
+
+public class FindingNemo
+{
+    
+    public void findNemo(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+
+        for(int i = 0; i < words.Length; i++)
+        {
+            if (words[i] == "Nemo")
+            {
+                VerifyIfFindNemo(i + 1);
+                return;
+            }
+        }
+        VerifyIfFindNemo(0);
+    }
+
+    private void VerifyIfFindNemo(int result)
+    {
+        if (result > 0)
+        {
+            Console.WriteLine($"I found Nemo at {result} !");
+        }else Console.WriteLine("I can't find Nemo :( .");
+    }
+}
