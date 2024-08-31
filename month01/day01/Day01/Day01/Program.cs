3c1e617329b38421ea287b9ba99e3cd1164b7529
zArthurOS <arthur426santos@gmail.com>
2024-08-31 19:30:02 -0300

    Add project files.

---
+++ month01/day01/Day01/Day01/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Enter your age: ");
+try
+{
+    var result = new AgeCalculator().AgeToDays(Console.ReadLine());
+    Console.Write($"You are: {result} days old.");
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Something went wrong.");
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
+
+public class AgeCalculator
+{
+    const int AGE = 365;
+
+
+    public int AgeToDays(int yo)
+    {
+        if (yo <= 0 || yo > int.MaxValue)
+        {
+            throw new Exception($"Enter a valid number(0 - {int.MaxValue})");
+        }
+        return yo * AGE;
+    }
+
+    public int AgeToDays(string getAge)
+    {
+        int.TryParse(getAge, out int result);
+        return AgeToDays(result);
+    }
+}
