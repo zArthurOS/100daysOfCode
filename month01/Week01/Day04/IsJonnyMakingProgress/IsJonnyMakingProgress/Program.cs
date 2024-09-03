982ca3b7c1275cfae687719cfbde224ef255a170
zArthurOS <arthur426santos@gmail.com>
2024-09-03 11:57:58 -0300

    IsJonnyMakingProgress

---
+++ month01/Week01/Day04/IsJonnyMakingProgress/IsJonnyMakingProgress/Program.cs
@@ -0,0 +1,17 @@
+﻿IsJonnyMakingProgress Jonny = new();
+int progress = Jonny.progressDays(new int[] { 9, 9 });
+
+Console.WriteLine($"Progress days: {progress}");
+
+Console.ReadKey();
+
+public class IsJonnyMakingProgress
+{
+    public int progressDays(int[] milesRunEverySaturday)
+    {
+        int result = 0;
+        for(int i = 0; i < milesRunEverySaturday.Length - 1; i++)
+            if (milesRunEverySaturday[i] < milesRunEverySaturday[i + 1]) result++;
+        return result;
+    }
+}
