5c02961e616774ef3d159e2d2404ca11bc9a2ea7
zArthurOS <arthur426santos@gmail.com>
2024-09-10 11:35:10 -0300

    SocketsPair

---
+++ month01/Week01/Day05/PairOfSocks/PairOfSocks/Program.cs
@@ -0,0 +1,30 @@
+﻿string sockets = "CABBACCC";
+
+int socketsPair = Sockets.findPairs(sockets);
+
+Console.WriteLine($"Found {socketsPair} pairs.");
+
+Console.ReadKey();
+
+
+public static class Sockets
+{
+    public static int findPairs(string sockets)
+    {
+        int SocketsPair = 0;
+        var list = new HashSet<char>(sockets);
+
+        foreach(var socket in list)
+        {
+            SocketsPair += CountPair(socket, sockets);
+        }
+
+        return SocketsPair / 2;
+    }
+
+    private static int CountPair(char socket, string sockets)
+    {
+        return sockets.Count(item => item == socket);
+    }
+
+}
