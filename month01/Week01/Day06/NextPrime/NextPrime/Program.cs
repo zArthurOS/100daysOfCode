0fbf11d668bc5ade33087d838b84c0efa139b41b
zArthurOS <arthur426santos@gmail.com>
2024-09-11 12:58:59 -0300

    NextPrime

---
+++ month01/Week01/Day06/NextPrime/NextPrime/Program.cs
@@ -0,0 +1,26 @@
+﻿
+NextPrime.Run(24);
+
+Console.ReadKey();
+
+public static class NextPrime
+{
+    public static void Run(int input)
+    {
+        while (!IsPrime(input)) input++;
+        Console.WriteLine($"Next Prime: {input}");
+    }
+
+    private static bool IsPrime(int n)
+    {
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
