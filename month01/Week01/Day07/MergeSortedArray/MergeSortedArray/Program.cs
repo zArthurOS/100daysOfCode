77fb6d2ddf4c8cf734ec98f9b7e8e0072cb139c5
zArthurOS <arthur426santos@gmail.com>
2024-09-12 09:00:11 -0300

    MergeSortedArray

---
+++ month01/Week01/Day07/MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[] arr1 = new int[] {1, 2, 3, 0, 0, 0};
+
+int[] arr2 = new int[] { 2, 5, 6 };
+
+GFG.mergeArrays(arr1, arr2);
+
+
+Console.WriteLine("Array after merging");
+for (int i = 0; i < arr1.Length; i++)
+    Console.Write(arr1[i] + " ");
+
+
+Console.ReadKey();
+
+public class GFG
+{
+    public static void mergeArrays(int[] arr1, int[] arr2)
+    {
+        int n = 0;
+        for(int i = 0;i < arr1.Length; i++)
+        {
+            if (arr1[i] == 0)
+                arr1[i] = arr2[n++];
+        }
+        
+
+        // sort the whole array arr3
+        Array.Sort(arr1);
+    }
+}
